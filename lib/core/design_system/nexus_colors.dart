3f88c54769367c40683804ba4f20ca162d27839a
matheusamaro-dev <matheus.amaro.147@gmail.com>
2026-07-30 13:15:24 -0300

    feat: establish Nexus design system foundation

---
+++ lib/core/design_system/nexus_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+abstract final class NexusColors {
+  static const brand = Color(0xFF00BFA6);
+
+  static const income = Color(0xFF3DDC97);
+  static const expense = Color(0xFFFF6B6B);
+  static const warning = Color(0xFFFFC857);
+  static const info = Color(0xFF5AA9FF);
+
+  static const darkBackground = Color(0xFF0B0F12);
+  static const darkSurface = Color(0xFF10161A);
+  static const darkSurfaceElevated = Color(0xFF161D22);
+  static const darkOutline = Color(0xFF273139);
+
+  static const lightBackground = Color(0xFFF6F8F9);
+  static const lightSurface = Color(0xFFFFFFFF);
+  static const lightOutline = Color(0xFFDDE3E7);
+}
---
+++ lib/core/design_system/nexus_radius.dart
@@ -0,0 +1,7 @@
+abstract final class NexusRadius {
+  static const double sm = 10;
+  static const double md = 14;
+  static const double lg = 18;
+  static const double xl = 22;
+  static const double pill = 999;
+}
